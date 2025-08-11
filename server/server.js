934d6f33e64eb55cb1978fe9dc91856bbe2b6942
Marcel Ramos <marceldavid@hotmail.com>
2025-08-11 18:30:05 -0400

    API set

---
+++ server/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors"; // <-- CORS global
+
+const app = express();
+app.use(cors());
+
+app.get("/api/artist", async (req, res) => {
+  const q = req.query.q || "queen";
+  const URL = `https://musicbrainz.org/ws/2/artist?query=${encodeURIComponent(
+    q
+  )}&fmt=json`;
+
+  try {
+    const mbRes = await fetch(URL, {
+      headers: {
+        "User-Agent": "MyMusicApp/1.0 (marceldramos@gmail.com)",
+      },
+    });
+
+    const data = await mbRes.json();
+
+    // Enablee CORS
+    res.set("Access-Control-Allow-Origin", "*");
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ error: "Proxy error", details: String(err) });
+  }
+});
+
+app.listen(4000, () => console.log("Proxy on http://localhost:4000"));
--- src/App.js
+++ src/App.js
@@ -1,4 +1,40 @@
+import { useEffect, useState } from "react";
+
+const BASE_URL = "http://localhost:4000/api";
+
 function App() {
+  const [content, setContent] = useState([]);
+
+  useEffect(function () {
+    async function fetchContent() {
+      try {
+        const res = await fetch(`${BASE_URL}/artist?q=queen`);
+
+        if (!res.ok) throw new Error("HTTP " + res.status);
+        const data = await res.json();
+
+        setContent(data);
+
+        const rows = data.artists.map((a) => ({
+          name: a.name,
+          type: a.type,
+          country: a.country,
+          disambiguation: a.disambiguation,
+          id: a.id,
+          score: a.score,
+        }));
+
+        console.table(rows); // fica legível no console
+      } catch (e) {
+        console.error("Error when fetching data", e);
+      }
+    }
+    fetchContent();
+  }, []);
+
+  console.log(content);
+
+  ////////////////////////////////////////////
   return (
     <div className="App">
       <p>My app</p>
