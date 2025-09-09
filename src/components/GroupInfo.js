e330959127309bd7a9a1cde751e679be2d8c42dd
Marcel Ramos <marceldavid@hotmail.com>
2025-09-09 16:47:52 -0400

    CSS background scroll bar  issue fixed

--- src/components/GroupInfo.js
+++ src/components/GroupInfo.js
@@ -27,52 +27,57 @@ function GroupInfo({ details, selected, albums, coverArt, about, content }) {
   }, [selected]);
 
   return (
-    <div className="group-info-container">
-      {selected && <ImageCarousel images={images} title={details?.name} />}
-      <li className="group-info">
-        {selected && (
-          <>
-            <h3 id="name">{details?.name}</h3>
+    <>
+      {selected && (
+        <div className="group-info-container">
+          {selected && <ImageCarousel images={images} title={details?.name} />}
+          <li className="group-info">
+            {selected && (
+              <>
+                <h3 id="name">{details?.name}</h3>
 
-            <div className="info-country">
-              <label>Country:</label>
-              <p>{details?.area?.name || ""}</p>
-            </div>
-            <div className="founded">
-              <label>Founded:</label>
-              <p>
-                {new Date(details?.["life-span"]?.begin).getFullYear() || "-"}
-              </p>
-            </div>
-            <div className="activity">
-              {details?.["life-span"]?.ended && (
-                <>
-                  <label>Ceased Activity:</label>
+                <div className="info-country">
+                  <label>Country:</label>
+                  <p>{details?.area?.name || ""}</p>
+                </div>
+                <div className="founded">
+                  <label>Founded:</label>
                   <p>
-                    {new Date(details?.["life-span"]?.end).toLocaleDateString(
-                      "pt-BR"
-                    )}
+                    {new Date(details?.["life-span"]?.begin).getFullYear() ||
+                      "-"}
                   </p>
-                </>
-              )}
+                </div>
+                <div className="activity">
+                  {details?.["life-span"]?.ended && (
+                    <>
+                      <label>Ceased Activity:</label>
+                      <p>
+                        {new Date(
+                          details?.["life-span"]?.end
+                        ).toLocaleDateString("pt-BR")}
+                      </p>
+                    </>
+                  )}
 
-              {details?.["life-span"]?.ended || (
-                <>
-                  <label>Active:</label>
-                  <p>{details?.["life-span"]?.ended || "Yes"}</p>
-                </>
-              )}
-              <About about={about} content={content} selected={selected} />
-              <AlbumList
-                albums={albums}
-                selected={selected}
-                coverArt={coverArt}
-              />
-            </div>
-          </>
-        )}
-      </li>
-    </div>
+                  {details?.["life-span"]?.ended || (
+                    <>
+                      <label>Active:</label>
+                      <p>{details?.["life-span"]?.ended || "Yes"}</p>
+                    </>
+                  )}
+                  <About about={about} content={content} selected={selected} />
+                  <AlbumList
+                    albums={albums}
+                    selected={selected}
+                    coverArt={coverArt}
+                  />
+                </div>
+              </>
+            )}
+          </li>
+        </div>
+      )}
+    </>
   );
 }
 
